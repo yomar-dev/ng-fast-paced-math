d1249461063001c52ddda3dd9cb28b201ab30d73
Yomar Miranda <mirandayomar@gmail.com>
2021-08-21 17:48:48 -0500

    feat: create equation form.

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-equation',
   templateUrl: './equation.component.html',
-  styleUrls: ['./equation.component.scss']
+  styleUrls: ['./equation.component.scss'],
 })
 export class EquationComponent implements OnInit {
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  get a() {
+    return this.mathForm.value.a;
   }
 
+  get b() {
+    return this.mathForm.value.b;
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
 }
